4dda82a8f29e9d96c4e5e7554996b84563a85dac
sayakaw <syk_hnsr@yahoo.co.jp>
2019-05-23 23:32:19 +0900

    initial commit

---
+++ Swiper-template/assets/js/app.js
@@ -0,0 +1,16 @@
+// $(function () {
+//   //initialize swiper when document ready
+//   var mySwiper = new Swiper ('.swiper-container', {
+//     loop: true
+//   })
+// });
+
+
+var swiper = new Swiper('.swiper-container', {
+  // effect: 'cube', などでカスタマイズできる
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+    
+  }
+});
